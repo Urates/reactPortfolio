17db551a1ee68d34f65d88b4b201de0709613836
Urates <umakiva@gmail.com>
2024-02-29 14:35:52 +0200

    updated mobile view

--- src/components/MobileView/MobileCareers.js
+++ src/components/MobileView/MobileCareers.js
@@ -12,18 +12,18 @@ const MobileCareers = () => {
         <p>It is notably correct to assume that my current skillset matches at least 3 careers which I have named and described in this page. My journey with entrepreneurship started just few years after my birth which was followed by a career in Accountancy after my matric then I topped all of that with strong skills in Technology, full stack software development to be more precise. </p>
 
         <div>
-            <a href=''><img src={AccountancySlide} alt='Accountancy slide'/></a>
+            <a href=''><img src={AccountancySlide} alt='Accountancy slide' className='AccountancyHover'/></a>
             <p>Accountancy</p>
         </div>
 
         
-        <div>
-            <a href=''><img src={EntrepSlide} alt='Entrepreneurship slide'/></a>
+        <div> 
+            <a href=''><img src={EntrepSlide} alt='Entrepreneurship slide' className='EntrepreneurshipHover'/></a>
             <p>Entrepreneurship</p>
         </div>
 
         <div>
-            <a href=''><img src={SoftwareDevSlide} alt='Software development slide'/></a>
+            <a href=''><img src={SoftwareDevSlide} alt='Software development slide' className='Software Dev Hover'/></a>
             <p>Software development</p>
         </div>
     </div>
