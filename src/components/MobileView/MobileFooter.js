290f7af1e24146666d36b0406293805c5693650f
Urates <umakiva@gmail.com>
2024-03-01 12:35:26 +0200

    Mobile view  with footer

--- src/components/MobileView/MobileFooter.js
+++ src/components/MobileView/MobileFooter.js
@@ -13,8 +13,18 @@ const MobileFooter = () => {
   return (
 
   <div className="MobileFooterContainer">
-      
-                   
+    <ul>
+      <li><a href='https://www.facebook.com/profile.php?id=100009275363960'><img src={Facebok} alt='Facebook Icon'/></a></li>  
+      <li><a href='https://www.linkedin.com/in/uviwe-makiva-5aa888146/'><img src={Linkedin} alt='LinkedIn Icon'/></a></li>  
+      <li><a href='https://www.instagram.com/uviwe.makiva/'><img src={Instagram} alt='Instagram Icon'/></a></li>
+      <li><a href='https://twitter.com/Uviwe76044741'><img src={X} alt='X Icon'/></a></li>  
+      <li><a href='umakiva@gmail.commailto:uviwemakiva@gmail.com?subject=Web%20Based%20Query&body=Write%20your%20email%20body%20here'><img src={Gmail} alt='Gmail Icon'/></a></li>  
+    </ul>
+    <ul>
+      <li id='Copyright'>
+        <p>Copyright Reserved&copy; Uviwe Makiva 2024 </p>
+      </li>
+    </ul>
   </div>
     
   );
