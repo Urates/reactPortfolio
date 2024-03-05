a7961b579947f264dbcd40f7a2a077bbcc65676f
Urates <umakiva@gmail.com>
2024-03-05 09:38:30 +0200

    Careers textual intro

--- src/components/CareersSlide.js
+++ src/components/CareersSlide.js
@@ -9,6 +9,7 @@ const CareersSlide = () => {
   return (
     <div id="careers">
       <span><h1>Careers</h1><hr></hr></span>
+      <p className="Careers-paragraph">It is notably correct to assume that my current skillset matches at least 3 careers which I have named and described in this page. My journey with entrepreneurship started just few years after my birth which was followed by a career in Accountancy after my matric then I topped all of that with strong skills in Technology, full stack software development to be more precise. </p>
       <div className="main-container">
           <a href="">
           <div className="accountancyStyle">
